0d4172bdb4354237fb2225c4d73b4b89c602f784
bbaumann <zentiltoutou@gmail.com>
2014-05-09 16:41:58 +0200

    Allow the user to display only not finished release in the admin release list. Allow the user to display only projects from a certain category in the admin release.

--- admin/menu_release.php
+++ admin/menu_release.php
@@ -9,18 +9,59 @@ function wpmanga_listReleases() {
 	
 	$projects = $wpdb->get_results("SELECT * FROM `{$wpdb->prefix}projects` ORDER BY `title` ASC");
 	
+	$publishedstatus = wpmanga_get('wpmanga_release_statuspublished',0);
 	if ($projects) {
 ?>
 		<div class="wrap">
 			<?php screen_icon('edit-pages'); ?>
 			<h2>Releases <a href="?page=manga/release" class="add-new-h2">Add a New Release</a></h2>
 			
+
+		<script type="text/javascript">
+			function toggleFinishedRelease(cb) {
+			  if (cb.checked)
+				jQuery('.finished').css( "display", "table-row" );
+			  else
+				jQuery('.finished').css( "display", "none" );
+			}
+
+			function catFilter(cbo) {
+			  var category = cbo.options[cbo.selectedIndex].value;
+			  if (category != '')
+			  {
+				jQuery('.project').css( "display", "none" );
+				jQuery('.cat'+category).css( "display", "block" );
+			  }
+			  else
+			  {
+				jQuery('.project').css( "display", "block" );
+			  }
+			}
+		</script>
+
+<?php if ($publishedstatus != 0)
+	  {
+?>
+		<label><input type="checkbox" onclick="toggleFinishedRelease(this);" checked>Show finished releases</label><br />
+<?php } ?>
+		<span>Show only projects from this category </span>
+		<select name="category" id="categoryCombo" onchange="catFilter(this)" >
+				<?php
+					$categories = get_sListCategories();
+					echo "<option value='' selected=\"selected\">None</option>";
+					foreach ($categories as $category) {
+						echo "<option value='{$category->id}'>{$category->name}</option>";
+					}
+				?>
+		</select>
+
 <?php
 			foreach ($projects as $project) {
 				$releases = $wpdb->get_results($wpdb->prepare("SELECT * FROM `{$wpdb->prefix}projects_releases` WHERE `project_id` = '%d' ORDER BY `volume` ASC, `chapter` ASC, `subchapter` ASC, `type` ASC", $project->id));
 				
 				if ($releases) {
 ?>
+					<div class="project cat<?php echo $project->category; ?>">
 					<br> &nbsp; <a href="admin.php?page=manga/project&action=edit&id=<?php echo $project->id; ?>" style="text-decoration: none; font-weight: bold"><?php echo $project->title; ?></a> &nbsp; <?php if ($project->title_alt) echo "&#12302;{$project->title_alt}&#12303;"; ?>
 					<table class="wp-list-table widefat fixed">
 						<thead>
@@ -31,8 +72,13 @@ function wpmanga_listReleases() {
 						
 						<tbody id="the-list">
 							<?php $row = 1; ?>
-							<?php foreach ($releases as $release) { ?>
-							<tr<?php if ($row % 2) echo ' class="alternate"'; $row++ ?>>
+							<?php foreach ($releases as $release) {
+									$class = "";
+									if ($publishedstatus != 0 && $release->status == $publishedstatus) 
+										$class = "finished";
+										?>
+							<tr<?php if ($row % 2) $class.=" alternate";
+									echo " class=\"{$class}\""; $row++ ?>>
 								<td><?php echo date('Y.m.d', $release->unixtime); ?></td>
 								<td><?php echo get_sFormatRelease($project, $release); if ($release->title) echo ' - <i>' . $release->title . '</i>'; ?></td>
 								<td>
@@ -44,6 +90,7 @@ function wpmanga_listReleases() {
 							<?php } ?>
 						</tbody>
 					</table>
+					</div>
 <?php
 				}
 			}
