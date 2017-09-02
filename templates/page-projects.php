57e19651b7dc9ceb1afa7325d9dcb93261b0400e
Albirew <rad1cal_edward@yahoo.fr>
2017-09-02 13:30:50 +0200

    Changed Rated (US) to PEGI (EU) in project list

--- templates/page-projects.php
+++ templates/page-projects.php
@@ -69,7 +69,7 @@
 			
 			foreach ($projects as $project) {
 				echo "<div class='item'><a href='" . get_sPermalink($project) . "'><img src='" . get_sThumbnail($thumbnail_width . "x" . $thumbnail_height, empty($project->image_thumbnail)?$project->image:$project->image_thumbnail) . "' width='{$thumbnail_width}' height='{$thumbnail_height}' id='{$project->id}' title='{$project->name}' alt='{$project->name}'>";
-				if ($project->mature) echo "<div class='mature'>R-18</div>";
+				if ($project->mature) echo "<div class='mature'>PEGI-18</div>";
 				echo "</a></div>";
 			}
 			
